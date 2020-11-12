e2f5c57ebc98e99e37b30dcc9d11e6e154d32c6f
theiskaa <59066341+theiskaa@users.noreply.github.com>
2020-11-12 18:34:56 +0400

    new logo

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -62,6 +62,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       key: scaffoldKey,
       appBar: buildCustomAppBar(),
       drawer: buildDrawer(),
